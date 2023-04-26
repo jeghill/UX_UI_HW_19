c40ce493e3875f24ad291633c9b5afbf4b6f9e30
jeghill <118201058+jeghill@users.noreply.github.com>
2023-04-26 21:04:27 +0100

    failed attemps at slowing it down

--- nav.js
+++ nav.js
@@ -1,7 +1,7 @@
 
 
 
-$("#menuIcon").on("click", function (){
+/*$("#menuIcon").on("click", function (){
     $(".navContainer").css("display", "flex");
     $("#menuIcon").css("display", "none");
     $("#crossIcon").css("display", "block");
@@ -9,6 +9,24 @@ $("#menuIcon").on("click", function (){
 
 $("#crossIcon").on("click", function (){
     $(".navContainer").css('display', 'none');
+    $("#crossIcon").css('display', 'none');
+    $("#menuIcon").css('display', 'block');
+});*/
+
+/*$("#menuIcon").on("click", function (){
+    $(".navContainer").show("slow");
+});*/
+
+$("#menuIcon").on("click", function (){
+    $("#menuIcon").css("display", "none");
+    $("#crossIcon").css("display", "block");
+});
+
+$(".ToggleIcon").on("click", function(){
+    $(".navContainer").toggle("ease");
+});
+
+$("#crossIcon").on("click", function (){
     $("#crossIcon").css('display', 'none');
     $("#menuIcon").css('display', 'block');
 });
