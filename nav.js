ff5ebff8c85caf7b6a8a36b37bb2dcfb1c59fa54
jeghill <118201058+jeghill@users.noreply.github.com>
2023-04-27 14:10:38 +0100

    added slow toggle
    added slow toggle - had to change jQuery library and it worked!!

--- nav.js
+++ nav.js
@@ -1,29 +1,11 @@
 
-
-
-/*$("#menuIcon").on("click", function (){
-    $(".navContainer").css("display", "flex");
-    $("#menuIcon").css("display", "none");
-    $("#crossIcon").css("display", "block");
-});
-
-$("#crossIcon").on("click", function (){
-    $(".navContainer").css('display', 'none');
-    $("#crossIcon").css('display', 'none');
-    $("#menuIcon").css('display', 'block');
-});*/
-
-/*$("#menuIcon").on("click", function (){
-    $(".navContainer").show("slow");
-});*/
-
 $("#menuIcon").on("click", function (){
     $("#menuIcon").css("display", "none");
     $("#crossIcon").css("display", "block");
 });
 
 $(".ToggleIcon").on("click", function(){
-    $(".navContainer").toggle("ease");
+    $(".navContainer").slideToggle(500);
 });
 
 $("#crossIcon").on("click", function (){
